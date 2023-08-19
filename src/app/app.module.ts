f0c5f5bf794a4a593109b1be4646354367f179e0
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2023-08-20 01:55:57 +0200

    adds navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { DrawingsComponent } from './drawings/drawings.component';
+import { AnimationsComponent } from './animations/animations.component';
+import { PhotographyComponent } from './photography/photography.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutMeComponent,
+    DrawingsComponent,
+    AnimationsComponent,
+    PhotographyComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/drawings/drawings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drawings',
+  templateUrl: './drawings.component.html',
+  styleUrls: ['./drawings.component.scss']
+})
+export class DrawingsComponent {
+
+}
