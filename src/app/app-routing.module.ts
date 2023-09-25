1451bb08298948bc9480d8d4965923b30db1497d
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2023-09-25 20:15:14 +0200

    fixes animation stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import {AboutMeComponent} from "./about-me/about-me.component";
 import {ArtworkComponent} from "./artworks/artwork.component";
 import {AnimationComponent} from "./animations/animation.component";
 import {PhotographyComponent} from "./photography/photography.component";
-import {MoreComponent} from "./more/more.component";
 import {ModelsComponent} from "./models/models.component";
 
 const routes: Routes = [
@@ -27,10 +26,6 @@ const routes: Routes = [
   {
     path: "photography",
     component: PhotographyComponent
-  },
-  {
-    path: "more",
-    component: MoreComponent
   },
   {
     path: "",
